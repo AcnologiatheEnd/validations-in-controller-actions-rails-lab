2cd98ef20a27875783753e897a4ebd50ef1521f8
Winston  Chung <18.chung.winston@ctkprep.org>
2020-02-03 09:58:29 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    
-    if @post.valid?
-      @post.update(post_params)
+     if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+   # if @post.valid?
+    #  @post.update(post_params)
 
     #  redirect_to post_path(@post)
    # else 
